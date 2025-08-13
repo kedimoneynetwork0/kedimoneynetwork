a5b167cf05249d36165685adecd5f70a329d1730
kedimoneynetwork0 <kedimoneynetworkl@gmail.com>
2025-08-13 14:11:17 +0200

    Update vite.config.js to use frontend as root directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: resolve(__dirname),
+  root: './frontend',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
   },
   plugins: [react()],
   server: {
