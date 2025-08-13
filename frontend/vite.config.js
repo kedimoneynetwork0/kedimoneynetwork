7c4872c5247fb49c42705f5aba55f57ab41ab745
kedimoneynetwork0 <kedimoneynetworkl@gmail.com>
2025-08-13 12:52:58 +0200

    Simplify vite.config.js to use default configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: resolve(__dirname),
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
   plugins: [react()],
   server: {
     port: 5173,
@@ -23,11 +12,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
     },
   },
 });
